3d4084c7dbd3264cde304c2f25de3616d72736ff
Richo <daininoricho@yahoo.com>
2022-12-06 12:30:27 +0700

    fix: blank space when there's no hero

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,43 +1,11 @@
 <!-- Hero -->
-<section id="default-carousel" class="relative" data-carousel="static">
-	<!-- Carousel wrapper -->
-	<div class="relative h-56 overflow-hidden md:h-screen">
-		<!-- Item 1 -->
-		<div class="hidden duration-1000 ease-in-out" data-carousel-item>
-			<div class="relative bg-[url(<?= base_url("images/foto latte.jpg") ?>)] bg-cover bg-center bg-no-repeat bg-left-bottom bg-110%">
-				<div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/5"></div>
-
-				<div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
-					<div class="max-w-xl text-center sm:text-left">
-						<h1 class="text-3xl font-extrabold sm:text-5xl">
-							Enjoy the finest
-
-							<strong class="block font-extrabold text-brown">
-								Coffee in Bali.
-							</strong>
-						</h1>
-
-						<p class="mt-4 max-w-lg sm:text-xl sm:leading-relaxed">
-							Try our Coffee Latte, created with love and care to give you the best coffee experience.
-						</p>
-
-
-						<!-- Button -->
-						<div class="mt-8 flex flex-wrap gap-4 text-center">
-							<a href="/menu" class="block w-full rounded-full bg-brown-md px-12 py-3 text-sm font-md text-white shadow hover:bg-brown focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto font-semibold text-base">
-								Order now
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Item 2 -->
-		<?php if (!empty($hero) && is_array($hero)) : ?>
+<?php if (!empty($hero) && is_array($hero)) : ?>
+	<section id="default-carousel" class="relative" data-carousel="static">
+		<!-- Carousel wrapper -->
+		<div class="relative h-56 overflow-hidden md:h-screen">
 			<?php foreach ($hero as $hero_item) : ?>
 				<div class="hidden duration-1000 ease-in-out" data-carousel-item>
-					<div class="relative bg-[url(<?= esc($hero_item['image']) ?>)] bg-cover bg-center bg-no-repeat bg-left-bottom bg-110%">
+					<div class="relative bg-[url(<?= esc($hero_item['image']) ?>)] bg-cover bg-center bg-no-repeat">
 						<div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/5"></div>
 
 						<div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
@@ -61,40 +29,34 @@
 					</div>
 				</div>
 			<?php endforeach ?>
-		<?php else : ?>
-			<h3>No Hero</h3>
-			<p>Unable to find any hero.</p>
-		<?php endif ?>
-	</div>
+		</div>
 
-	<!-- Slider indicators -->
-	<div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-		<button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-		<?php if (!empty($hero) && is_array($hero)) : ?>
+		<!-- Slider indicators -->
+		<div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
 			<?php foreach ($hero as $key => $hero_item) : ?>
-				<button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide <?= $key + 2 ?>" data-carousel-slide-to="<?= $key + 1 ?>"></button>
+				<button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide <?= $key + 1 ?>" data-carousel-slide-to="<?= $key ?>"></button>
 			<?php endforeach ?>
-		<?php endif ?>
-	</div>
+		</div>
 
-	<!-- Slider controls -->
-	<button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-		<span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-			<svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
-			</svg>
-			<span class="sr-only">Previous</span>
-		</span>
-	</button>
-	<button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-		<span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-			<svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-			</svg>
-			<span class="sr-only">Next</span>
-		</span>
-	</button>
-</section>
+		<!-- Slider controls -->
+		<button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+			<span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+				<svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+				</svg>
+				<span class="sr-only">Previous</span>
+			</span>
+		</button>
+		<button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+			<span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+				<svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+				</svg>
+				<span class="sr-only">Next</span>
+			</span>
+		</button>
+	</section>
+<?php endif ?>
 
 <!-- Menu -->
 <section class="bg-white">
