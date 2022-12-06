acd8a9e47cb5b64510d403fd7d3da867ead2adf9
Richo <daininoricho@yahoo.com>
2022-12-06 12:19:10 +0700

    feat: adding migration and seeder for table hero

---
+++ app/Database/Migrations/2022-12-06-034403_Hero.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Hero extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type'       => 'TEXT',
+            ],
+            'desc' => [
+                'type' => 'VARCHAR',
+                'constraint' => '128',
+                'null' => true,
+            ],
+            'cta_title' => [
+                'type' => 'VARCHAR',
+                'constraint' => '32',
+                'null' => true,
+            ],
+            'cta_ref' => [
+                'type' => 'VARCHAR',
+                'constraint' => '128',
+                'null' => true,
+            ],
+            'image' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at DATETIME DEFAULT CURRENT_TIMESTAMP',
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('hero');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('hero');
+    }
+}
