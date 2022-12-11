0c1c5110b632a3ae9f4c28abe43d622667642ca8
Richo <daininoricho@yahoo.com>
2022-12-12 04:44:21 +0700

    feat: product details and hero controller

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -32,4 +32,43 @@ class Admin extends BaseController
             . view('pages/admin/product')
             . view('templates/tail');
     }
+
+    public function productByID($id)
+    {
+        $productModel = model(ProductModel::class);
+        $product = $productModel->getProduct($id);
+        $data['product'] = $product;
+        $data['selected'] = 'product';
+
+        return view('templates/header', $data)
+        . view('templates/admin/sidebar')
+        . view('pages/admin/productByID')
+        . view('templates/tail');
+    }
+
+    public function hero()
+    {
+        $heroModel = model(ProductModel::class);
+        $hero = $heroModel->getProduct();
+        $data['hero'] = $hero;
+        $data['selected'] = 'hero';
+
+        return view('templates/header', $data)
+            . view('templates/admin/sidebar')
+            . view('pages/admin/hero')
+            . view('templates/tail');
+    }
+
+    public function heroByID($id)
+    {
+        $heroModel = model(ProductModel::class);
+        $hero = $heroModel->getProduct($id);
+        $data['hero'] = $hero;
+        $data['selected'] = 'hero';
+
+        return view('templates/header', $data)
+        . view('templates/admin/sidebar')
+        . view('pages/admin/heroByID')
+        . view('templates/tail');
+    }
 }
