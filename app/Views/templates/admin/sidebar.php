36912e25d5c50d38c5313ea6abed3bdf5548dfa9
Richo <daininoricho@yahoo.com>
2022-12-12 05:04:49 +0700

    feat: change sidebar font color

--- app/Views/templates/admin/sidebar.php
+++ app/Views/templates/admin/sidebar.php
@@ -7,14 +7,14 @@
             </div>
 
             <nav aria-label="Main Nav" class="flex flex-col space-y-1">
-                <a href="/admin/product" class="flex items-center rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700 <?php if (!empty($selected) && $selected === 'product') echo ('text-gray-700 bg-gray-100') ?>">
-                    <img class="h-5 w-5 opacity-75" src="<?= base_url("icon/fast-food.png") ?>">
+                <a href="/admin/product" class="flex items-center rounded-lg px-4 py-2 text-brown-md hover:bg-gray-100 hover:text-brown <?php if (!empty($selected) && $selected === 'product') echo ('text-brown bg-gray-100') ?>">
+                    <img class="h-5 w-5 opacity-75" src="<?= base_url("icon/fast-food.png") ?>" style="filter: invert(17%) sepia(45%) saturate(1490%) hue-rotate(343deg) brightness(92%) contrast(93%);">
 
                     <span class="ml-3 text-sm font-medium"> Products </span>
                 </a>
 
-                <a href="/admin/hero" class="flex items-center rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700 <?php if (!empty($selected) && $selected === 'hero') echo ('text-gray-700 bg-gray-100') ?>">
-                    <img class="h-5 w-5 opacity-75" src="<?= base_url("icon/slider.png") ?>">
+                <a href="/admin/hero" class="flex items-center rounded-lg px-4 py-2 text-brown-md hover:bg-gray-100 hover:text-brown <?php if (!empty($selected) && $selected === 'hero') echo ('text-brown bg-gray-100') ?>">
+                    <img class="h-5 w-5 opacity-75" src="<?= base_url("icon/slider.png") ?>" style="filter: invert(17%) sepia(45%) saturate(1490%) hue-rotate(343deg) brightness(92%) contrast(93%);">
 
                     <span class="ml-3 text-sm font-medium"> Hero / Banner </span>
                 </a>
@@ -23,13 +23,13 @@
 
         <div class="sticky bottom-0 border-t border-gray-100">
             <a href="#" class="flex shrink-0 items-center bg-white p-4 hover:bg-gray-50">
-                <img alt="Man" src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" class="h-10 w-10 rounded-full object-cover" />
+                <img alt="Man" src="<?= base_url('/images/foto richo.jpeg') ?>" class="h-10 w-10 rounded-full object-cover" />
 
                 <div class="ml-1.5">
                     <p class="text-xs">
-                        <strong class="block font-medium">Eric Frusciante</strong>
+                        <strong class="block font-medium">Richo Dainino</strong>
 
-                        <span> eric@frusciante.com </span>
+                        <span>daininoricho@yahoo.com</span>
                     </p>
                 </div>
             </a>
