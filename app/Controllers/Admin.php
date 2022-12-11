e80327ef1d69fdf7d8e5a118b3d7b15f90f7adfb
Richo <daininoricho@yahoo.com>
2022-12-11 18:40:31 +0700

    feat: dashboard and product controller

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -9,18 +9,27 @@ class Admin extends BaseController
     public function index()
     {
         return view('templates/header')
-            . view('admin/login')
+            . view('pages/admin/login')
             . view('templates/tail');
     }
 
     public function dashboard()
     {
+        return view('templates/header')
+            . view('pages/admin/dashboard')
+            . view('templates/tail');
+    }
+
+    public function product()
+    {
         $productModel = model(ProductModel::class);
         $product = $productModel->getProduct();
         $data['product'] = $product;
+        $data['selected'] = 'product';
 
         return view('templates/header', $data)
-            . view('admin/dashboard')
+            . view('templates/admin/sidebar')
+            . view('pages/admin/product')
             . view('templates/tail');
     }
 }
