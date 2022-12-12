fedf8838fb0f593e13c00e41b4ef698dda28bcb8
Richo <daininoricho@yahoo.com>
2022-12-12 20:22:33 +0700

    feat: adding updated_at and deleted_at on db migration

--- app/Database/Migrations/2022-12-06-034403_Hero.php
+++ app/Database/Migrations/2022-12-06-034403_Hero.php
@@ -43,6 +43,8 @@ class Hero extends Migration
                 'constraint' => '255',
             ],
             'created_at DATETIME DEFAULT CURRENT_TIMESTAMP',
+            'updated_at DATETIME DEFAULT CURRENT_TIMESTAMP',
+            'deleted_at DATETIME DEFAULT NULL',
         ]);
         $this->forge->addKey('id', true);
         $this->forge->createTable('hero');
--- app/Database/Migrations/2022-12-06-074400_Product.php
+++ app/Database/Migrations/2022-12-06-074400_Product.php
@@ -40,6 +40,8 @@ class Product extends Migration
                 'constraint' => '255',
             ],
             'created_at DATETIME DEFAULT CURRENT_TIMESTAMP',
+            'updated_at DATETIME DEFAULT CURRENT_TIMESTAMP',
+            'deleted_at DATETIME DEFAULT NULL',
         ]);
         $this->forge->addKey('id', true);
         $this->forge->createTable('product');
