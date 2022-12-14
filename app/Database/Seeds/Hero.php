0d0841d2655d493758d85e62a300f29689899d88
Richo <daininoricho@yahoo.com>
2022-12-15 01:41:27 +0700

    refactor: change hero seeder images

--- app/Database/Seeds/Hero.php
+++ app/Database/Seeds/Hero.php
@@ -19,7 +19,7 @@ class Hero extends Seeder
                 'desc'  => 'Try our Coffee Latte, created with love and care to give you the best coffee experience.',
                 'cta_title' => 'Order now',
                 'cta_ref' => '/menu/1',
-                'image' => 'https://images.unsplash.com/photo-1571658734974-e513dfb8b86b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=875&q=80',
+                'image' => 'latte.webp',
             ],
             [
                 'status' => 'Approved',
@@ -27,7 +27,7 @@ class Hero extends Seeder
                 'desc' => 'Fresh prawn with an eggcellent flavor of a salted egg.',
                 'cta_title' => 'Try me',
                 'cta_ref' => '/menu/2',
-                'image' => 'https://images.unsplash.com/photo-1504309250229-4f08315f3b5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+                'image' => 'shrimp.webp',
             ],
             [
                 'status' => 'Approved',
@@ -35,7 +35,7 @@ class Hero extends Seeder
                 'desc'    => 'We got a delicious and beautiful matcha latte that you can enjoy with every sip of it.',
                 'cta_title' => 'Order me',
                 'cta_ref' => '/menu/3',
-                'image' => 'https://images.unsplash.com/photo-1561658286-ecb9fe9d8480?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+                'image' => 'matcha latte.webp',
             ]
         ];
 
